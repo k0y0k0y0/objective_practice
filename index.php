247224df441f7e83213ec45216eefa7c19e08b4e
taak <papipupepo_10@yahoo.co.jp>
2020-10-07 22:56:00 +0900

    Add  required files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//ログ設定
+ini_set('log_errors', 'on');
+ini_set('error_log', 'php.log');
+//セッション使用
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  
+</body>
+</html>
